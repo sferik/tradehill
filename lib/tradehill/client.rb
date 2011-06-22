0f414e3c61830f09954564d91a52823bada4776f
Erik Michaels-Ober <sferik@gmail.com>
2011-06-21 17:26:24 -0700

    Use versioned API path

--- lib/tradehill/client.rb
+++ lib/tradehill/client.rb
@@ -12,7 +12,7 @@ module TradeHill
     # @example
     #   TradeHill.asks
     def asks
-      get('/API/USD/Orderbook')['asks'].each do |o|
+      get('/APIv1/USD/Orderbook')['asks'].each do |o|
         o[0] = o[0].to_f
         o[1] = o[1].to_f
       end
@@ -24,7 +24,7 @@ module TradeHill
     # @example
     #   TradeHill.bids
     def bids
-      get('/API/USD/Orderbook')['bids'].each do |o|
+      get('/APIv1/USD/Orderbook')['bids'].each do |o|
         o[0] = o[0].to_f
         o[1] = o[1].to_f
       end
@@ -36,7 +36,7 @@ module TradeHill
     # @example
     #   TradeHill.trades
     def trades
-      get('/API/USD/Trades').each do |t|
+      get('/APIv1/USD/Trades').each do |t|
         t['date'] = Time.at(t['date'].to_i)
         t['price'] = t['price'].to_f
         t['amount'] = t['amount'].to_f
--- spec/tradehill/client_spec.rb
+++ spec/tradehill/client_spec.rb
@@ -7,39 +7,39 @@ describe TradeHill::Client do
 
   describe '#asks' do
     before do
-      stub_get('/API/USD/Orderbook').
+      stub_get('/APIv1/USD/Orderbook').
         to_return(:status => 200, :body => fixture('orderbook.json'))
     end
 
     it "should fetch open asks" do
       asks = @client.asks
-      a_get('/API/USD/Orderbook').should have_been_made
+      a_get('/APIv1/USD/Orderbook').should have_been_made
       asks.last.should == [30.0, 1.0]
     end
   end
 
   describe '#bids' do
     before do
-      stub_get('/API/USD/Orderbook').
+      stub_get('/APIv1/USD/Orderbook').
         to_return(:status => 200, :body => fixture('orderbook.json'))
     end
 
     it "should fetch open bids" do
       bids = @client.bids
-      a_get('/API/USD/Orderbook').should have_been_made
+      a_get('/APIv1/USD/Orderbook').should have_been_made
       bids.last.should == [18.5, 75.0]
     end
   end
 
   describe '#trades' do
     before do
-      stub_get('/API/USD/Trades').
+      stub_get('/APIv1/USD/Trades').
         to_return(:status => 200, :body => fixture('trades.json'))
     end
 
     it "should fetch trades" do
       trades = @client.trades
-      a_get('/API/USD/Trades').should have_been_made
+      a_get('/APIv1/USD/Trades').should have_been_made
       trades.last.date.should == Time.utc(2011, 6, 16, 19, 07, 54)
       trades.last.price.should == 19.25
       trades.last.amount.should == 0.15
