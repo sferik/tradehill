fe5a617d7b6b9db9a5312fccad5210528da5931c
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 18:44:07 -0700

    Wrap all module methods in class << self

--- lib/tradehill.rb
+++ lib/tradehill.rb
@@ -6,22 +6,22 @@ module TradeHill
     def configure
       yield self
     end
-  end
 
-  # Alias for TradeHill::Client.new
-  #
-  # @return [TradeHill::Client]
-  def self.new
-    TradeHill::Client.new
-  end
+    # Alias for TradeHill::Client.new
+    #
+    # @return [TradeHill::Client]
+    def new
+      TradeHill::Client.new
+    end
 
-  # Delegate to TradeHill::Client
-  def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
-  end
+    # Delegate to TradeHill::Client
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
 
-  def self.respond_to?(method, include_private=false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
 end
