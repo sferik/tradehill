9b3377c095e15e0162c66866a9b4ecd5da53ec9d
Erik Michaels-Ober <sferik@gmail.com>
2011-07-14 02:55:25 -0700

    Fix broken error handling

--- lib/faraday/response/raise_tradehill_error.rb
+++ lib/faraday/response/raise_tradehill_error.rb
@@ -3,8 +3,8 @@ require 'faraday'
 module Faraday
   class Response::RaiseTradeHillError < Response::Middleware
     def on_complete(env)
-      if 200 == env[:status] && env[:body].is_a?(Hash) && env[:body][:error]
-        raise TradeHill::Error, env[:body][:error]
+      if 200 == env[:status] && env[:body].is_a?(Hash) && env[:body]['error']
+        raise TradeHill::Error, env[:body]['error']
       end
     end
   end
